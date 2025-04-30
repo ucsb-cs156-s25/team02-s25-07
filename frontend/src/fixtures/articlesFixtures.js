17f8a4960b44fa3eedfd7bc523f6874f1f679852
Shelly Zhu <zhixiuzhu@umail.ucsb.edu>
2025-04-29 17:32:26 -0700

    fix fixtures

--- frontend/src/fixtures/articlesFixtures.js
+++ frontend/src/fixtures/articlesFixtures.js
@@ -5,7 +5,7 @@ const articlesFixtures = {
     url: "https://www.baeldung.com/spring-boot-react-crud",
     explanation: "A tutorial on how to integrate React with Spring Boot",
     email: "phtcon@ucsb.edu",
-    dateAdded: "2022-01-02T12:00:00"
+    dateAdded: "2022-01-02T12:00:00",
   },
   threeArticles: [
     {
@@ -14,7 +14,7 @@ const articlesFixtures = {
       url: "https://www.baeldung.com/spring-boot-react-crud",
       explanation: "A tutorial on how to integrate React with Spring Boot",
       email: "phtcon@ucsb.edu",
-      dateAdded: "2022-01-02T12:00:00"
+      dateAdded: "2022-01-02T12:00:00",
     },
     {
       id: 2,
@@ -22,7 +22,7 @@ const articlesFixtures = {
       url: "https://spring.io/guides/gs/accessing-data-jpa/",
       explanation: "Official Spring guide for JPA",
       email: "cgaucho@ucsb.edu",
-      dateAdded: "2022-04-03T12:00:00"
+      dateAdded: "2022-04-03T12:00:00",
     },
     {
       id: 3,
@@ -30,9 +30,9 @@ const articlesFixtures = {
       url: "https://www.toptal.com/spring/spring-security-tutorial",
       explanation: "Comprehensive guide to Spring Security for REST APIs",
       email: "ldelplaya@ucsb.edu",
-      dateAdded: "2022-07-04T12:00:00"
-    }
-  ]
+      dateAdded: "2022-07-04T12:00:00",
+    },
+  ],
 };
 
 export { articlesFixtures };
