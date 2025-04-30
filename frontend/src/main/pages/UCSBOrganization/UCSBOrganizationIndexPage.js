c855085350edf2e54c06c58a12e9d81145575739
hannahz0 <85980913+hannahz0@users.noreply.github.com>
2025-04-29 18:17:37 -0700

    fixes

--- frontend/src/main/pages/UCSBOrganization/UCSBOrganizationIndexPage.js
+++ frontend/src/main/pages/UCSBOrganization/UCSBOrganizationIndexPage.js
@@ -7,10 +7,10 @@ export default function UCSBOrganizationIndexPage() {
       <div className="pt-2">
         <h1>Index page not yet implemented</h1>
         <p>
-          <a href="/placeholder/create">Create</a>
+          <a href="/ucsborganization/create">Create</a>
         </p>
         <p>
-          <a href="/placeholder/edit/1">Edit</a>
+          <a href="/ucsborganization/edit/1">Edit</a>
         </p>
       </div>
     </BasicLayout>
--- frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationEditPage.test.js
+++ frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationEditPage.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import PlaceholderEditPage from "main/pages/UCSBOrganization/UCSBOrganizationEditPage";
+import UCSBOrganizationEditPage from "main/pages/UCSBOrganization/UCSBOrganizationEditPage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 
