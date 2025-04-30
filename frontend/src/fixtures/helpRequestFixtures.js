2bbe44d50a827847b1524af3dc02cce043be87a7
Annika Damstedt <acdamstedt@ucsb.edu>
2025-04-29 18:11:21 -0700

    AD - used prettier to change formatting

--- frontend/src/fixtures/helpRequestFixtures.js
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -1,5 +1,15 @@
 const helpRequestFixtures = {
-    oneRequest: {
+  oneRequest: {
+    id: 1,
+    requesterEmail: "acdamstedt@ucsb.edu",
+    teamID: "07",
+    tableOrBreakoutRoom: "07",
+    localDateTime: "2022-01-02T12:00:00",
+    explanation: "Setting up fixtures not working",
+    solved: "true",
+  },
+  threeRequests: [
+    {
       id: 1,
       requesterEmail: "acdamstedt@ucsb.edu",
       teamID: "07",
@@ -8,36 +18,25 @@ const helpRequestFixtures = {
       explanation: "Setting up fixtures not working",
       solved: "true",
     },
-    threeRequests: [
-      {
-        id: 1,
-        requesterEmail: "acdamstedt@ucsb.edu",
-        teamID: "07",
-        tableOrBreakoutRoom: "07",
-        localDateTime: "2022-01-02T12:00:00",
-        explanation: "Setting up fixtures not working",
-        solved: "true",
-      },
-      {
-        id: 2,
-        requesterEmail: "acdamstedt@umail.ucsb.edu",
-        teamID: "01",
-        tableOrBreakoutRoom: "01",
-        localDateTime: "2022-02-02T12:00:00",
-        explanation: "Setting up fixtures really not working",
-        solved: "true",
-      },
-      {
-        id: 3,
-        requesterEmail: "acdamstedt@csil.cs.ucsb.edu",
-        teamID: "03",
-        tableOrBreakoutRoom: "03",
-        localDateTime: "2022-03-02T12:00:00",
-        explanation: "Setting up fixtures completely broken",
-        solved: "true",
-      },
-    ],
-  };
-  
-  export { helpRequestFixtures };
-  
+    {
+      id: 2,
+      requesterEmail: "acdamstedt@umail.ucsb.edu",
+      teamID: "01",
+      tableOrBreakoutRoom: "01",
+      localDateTime: "2022-02-02T12:00:00",
+      explanation: "Setting up fixtures really not working",
+      solved: "true",
+    },
+    {
+      id: 3,
+      requesterEmail: "acdamstedt@csil.cs.ucsb.edu",
+      teamID: "03",
+      tableOrBreakoutRoom: "03",
+      localDateTime: "2022-03-02T12:00:00",
+      explanation: "Setting up fixtures completely broken",
+      solved: "true",
+    },
+  ],
+};
+
+export { helpRequestFixtures };
