ce96864536af22f8d2b41182aebe42191332aa4e
Forrest Zhou <lzhou@ucsb.edu>
2025-04-29 17:45:21 -0700

    fz - added fixtures for menu items

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,30 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+    oneDiningCommonsMenuItem: {
+        "id": 2,
+        "diningCommonsCode": "Portola",
+        "name": "California Roll",
+        "station": "International"
+      },
+    threeDiningCommonsMenuItem: [
+        {
+          "id": 2,
+          "diningCommonsCode": "Portola",
+          "name": "California Roll",
+          "station": "International"
+        },
+        {
+          "id": 3,
+          "diningCommonsCode": "DLG",
+          "name": "Cheese Burger",
+          "station": "The brick"
+        },
+        {
+          "id": 4,
+          "diningCommonsCode": "Ortega",
+          "name": "Potato Chip",
+          "station": "none"
+        }
+      ],
+  };
+  
+  export { DiningCommonsMenuItem };
