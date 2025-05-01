4f69e2a2556c743d34d9c87eea071eb549ea97df
Hannya Yan <yilei_yan@ucsb.edu>
2025-04-30 18:40:15 -0700

    add tests for MIR placeholder pages (Create, Edit, Index)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -56,7 +56,11 @@ function App() {
         )}
         {hasRole(currentUser, "ROLE_USER") && (
           <>
-            <Route exact path="/menuitemreviews" element={<MenuItemReviewsIndexPage />} />
+            <Route
+              exact
+              path="/menuitemreviews"
+              element={<MenuItemReviewsIndexPage />}
+            />
           </>
         )}
         {hasRole(currentUser, "ROLE_ADMIN") && (
